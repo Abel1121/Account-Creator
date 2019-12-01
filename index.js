54c66e78fae8766dba4e2fe8ab766f16ed427b7f
Marek Kobyliński <Marecki1121@gmail.com>
2019-12-01 22:53:50 +0100

    add: nickname creator

---
+++ index.js
@@ -0,0 +1,16 @@
+const howMany = document.getElementById("howMany").value;
+const nicknameList = document.querySelector('.nickname_list');
+const submit = document.querySelector('.submit');
+const nickList = ['White', 'Red', 'Yellow', 'Blue', 'Black', 'Brown', 'Orange', 'Grey', 'Green', 'Gold', 'Silver', 'Bronze', 'Diamond', 'Platinium', 'Challenger', 'Knight', 'Tank', 'Archer', 'Palladin', 'Druid', 'Sorccer', 'Human', 'Goblin', 'Dragon', 'Angel', 'Unicorn', 'Minotaur', 'Golem', 'Devil', 'Demon', 'Giant', 'Titan', 'Energy', 'Power', 'Master', 'Slime', 'Orc'];
+console.log(howMany);
+console.log(nickList.length);
+console.log(nickList[1]+nickList[15]);
+
+submit.addEventListener('click', (e) => {
+    for (let i = 0; i < document.getElementById("howMany").value; i++){
+        let a = Math.random()*nickList.length;
+        let b = Math.random()*nickList.length;
+        console.log(nickList[Math.round(a)]+nickList[Math.round(b)])
+        nicknameList.innerHTML += `<ol>${nickList[Math.round(a)]+nickList[Math.round(b)]}</ol>`
+    }
+});
